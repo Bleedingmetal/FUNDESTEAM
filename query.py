c807668d4ed661ef32ce2e269eb866416a9528db
BleedingMetal <adityamanojkrishna2005@gmail.com>
2025-09-02 12:55:07 -0500

    trust in the query script and hope it doesnt die

--- query.py
+++ query.py
@@ -3,6 +3,8 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
+# load .env so API key is available
+load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 
@@ -15,3 +17,14 @@ client = OpenAI(api_key=api_key)
 if len(sys.argv) < 2:
     print("Usage: python query.py 'your question here'")
     sys.exit(1)
+
+question = " ".join(sys.argv[1:])
+
+#bro why is the docs lowkey so confusing
+
+response = client.responses.create(  #this acc to docs at least should mean that im no longer on the old format and hopefully it supports multimodal inputs later on
+    model="gpt-5-nano",
+    input=question,
+)
+
+print(response.output_text)
